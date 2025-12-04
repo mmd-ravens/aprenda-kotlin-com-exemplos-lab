74264485bdb21c33670a753555898a82015ea145
mmd <mamedehyuri@gmail.com>
2025-12-04 19:49:50 -0300

    Entrega da atividade e melhoria do código

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,34 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, DANASA }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg novoUsuario: Usuario) {
+        for (usuario in novoUsuario) {
+            inscritos.add(usuario)
+            println("${usuario.nome} foi matriculado")
+        }
+   fun matricularComFuncaoDeEscopoAlso(usuario: Usuario) {
+       usuario.also {
+           inscritos.add(it)
+           println("${it.nome} foi matriculado")
+       }
+   }
+       
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val f = Formacao("android", listOf(), Nivel.BASICO)
+    val user1 = Usuario("Hyu")
+    val user2 = Usuario("Ana")
+    
+    f.matricular(user1,user2)
 }
